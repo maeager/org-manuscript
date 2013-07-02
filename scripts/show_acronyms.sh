b557dda19b9cf29a34246ae2763b79d7b6dcd866
Michael Eager <mick.eager@gmail.com>
2013-07-02 18:23:52 +1000

    show_acronyms search for acronyms without preceding slash

--- scripts/show_acronyms.sh
+++ scripts/show_acronyms.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 org_root_path=$(dirname $0)
-acron=$(sed 's/\\[^{]*{\([^}]*\)}.*/\1/' $org_root_path/../misc/glossary.tex | grep -e '^\\' | tr -d '\\' | tr '\n' '|' ) 
-echo $acron
-egrep -i -Hn --color "\\b($acron)\\b" $@
+acron=$(sed 's/\\[^{]*{\([^}]*\)}.*/\1/' $org_root_path/../misc/glossary.tex | grep -e '^\\' | sed 's#\\#[^\\{]#'  | tr '\n' '|' ) 
+# echo $acron
+grep -Hn --color=auto -E "\\b($acron)\\b" $1
 
