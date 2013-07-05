e3105d79dbfd1f184e5ece1b8ef6cff2789e3633
Michael Eager <mick.eager@gmail.com>
2013-07-05 16:17:58 +1000

    Fix bug in show_acronyms.sh

--- scripts/show_acronyms.sh
+++ scripts/show_acronyms.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 org_root_path=$(dirname $0)
-acron=$(sed 's/\\[^{]*{\([^}]*\)}.*/\1/' $org_root_path/../misc/glossary.tex | grep -e '^\\' | sed 's#\\#[^\\{]#'  | tr '\n' '|' ) 
-# echo $acron
+acron=$(sed 's/\\[^{]*{\([^}]*\)}.*/\1/' $org_root_path/../misc/glossary.tex | grep -e '^\\' | sed 's#\\#[^\\\\{]#'  | tr '\n' '|' ) 
+acron=${acron%?}
 grep -Hn --color=auto -E "\\b($acron)\\b" $1
 
