60ef376ff6b946823314e7632eea36589632f27a
Michael Eager <mick.eager@gmail.com>
2013-07-20 07:48:19 +1000

    Strating work on acknowledgements.tex.  Fixing capitals in glossary.  Script show_acronyms fixed to show acronums without pre-ceeding slash - some conflicts with smaller labels within other labels.

--- scripts/show_acronyms.sh
+++ scripts/show_acronyms.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 org_root_path=$(dirname $0)
-acron=$(sed 's/\\[^{]*{\([^}]*\)}.*/\1/' $org_root_path/../misc/glossary.tex | grep -e '^\\' | sed 's#\\#[^\\\\{]#'  | tr '\n' '|' ) 
+acron=$(sed 's/\\[^{]*{\([^}]*\)}.*/\1/' $org_root_path/../misc/glossary.tex | grep -e '^\\' | sed 's_\\_[^\\\\:\{]_'  | tr '\n' '|' ) 
 acron=${acron%?}
-grep -Hn --color=auto -E "\\b($acron)\\b" $1
 
+grep  -Hn --color=auto -E "\\b($acron)\\b" $1
+echo $acron
