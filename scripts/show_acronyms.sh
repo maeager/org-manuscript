3cb1f708c4855d3a23ead86a7daf6677ec3927f2
Michael Eager <mick.eager@gmail.com>
2014-01-16 15:57:32 +1100

    General fixes to spacing in Chapter06.org

--- scripts/show_acronyms.sh
+++ scripts/show_acronyms.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
-
+# Show acronyms in glossary.tex in file
 org_root_path=$(dirname $0)
+
+# acron=$(sed 's/\\[^{]*{\([^}]*\)}.*/\1/' $org_root_path/../misc/glossary.tex | grep -e '^\\' | sed 's_\\_[^\\\\:\{]_'  | tr '\n' '|' ) 
 acron=$(sed 's/\\[^{]*{\([^}]*\)}.*/\1/' $org_root_path/../misc/glossary.tex | grep -e '^\\' | sed 's_\\_[^\\\\:\{]_'  | tr '\n' '|' ) 
+
 acron=${acron%?}
 
-grep  -Hn --color=auto -E "\\b($acron)\\b" $1
-echo $acron
+grep -Hn --color=auto -E "\\b($acron)\\b" $1
+#echo $acron
